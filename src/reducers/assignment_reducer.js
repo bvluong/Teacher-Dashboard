4841f09eddc064104701c422406f53df40b53b21
bvluong <bryantvinluong@gmail.com>
2017-08-07 15:55:23 -0700

    Update Reducer to filter Assignement Object for relevant information

--- src/reducers/assignment_reducer.js
+++ src/reducers/assignment_reducer.js
@@ -2,14 +2,23 @@ import { RECEIVE_ASSIGNEMENTS } from '../actions/assignment_actions';
 import { merge } from 'lodash';
 
 const defaultState = {
+
 };
 
 const assignmentReducer = (state = defaultState, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_ASSIGNEMENTS:
-      console.log(action);
-      return merge({}, defaultState, action );
+      let newState = {};
+      action.assignments.forEach( el => {
+        newState[el.id] = {
+          id: el.id,
+          title: el.title,
+          description: el.description,
+          due_at: el.due_at
+        };
+      });
+      return merge({}, defaultState, newState );
     default:
       return state;
   }
