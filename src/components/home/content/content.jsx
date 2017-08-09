0f9e579cb9b9adf533276b26e0bb69c7bc56ac14
bvluong <bryantvinluong@gmail.com>
2017-08-09 11:57:36 -0700

    Add content skeleton component

---
+++ src/components/home/content/content.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div className='content'>
+      </div>
+    );
+  }
+}
+
+export default Content;
